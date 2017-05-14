8d92f65826a24b5d1794b7cbe7576bc74eb2744b
Wing lee <mmmwhy@hust.edu.cn>
2017-05-14 19:12:52 +0800

    no message

--- sm_node.sh
+++ sm_node.sh
@@ -36,11 +36,11 @@ install_soft_for_each(){
 }
 install_nodejs(){
 	mkdir /usr/local/nodejs
-	wget -N --no-check-certificate https://nodejs.org/dist/v6.9.1/node-v6.9.1-linux-x86.tar.gz
-	tar -xf node-v6.9.1-linux-x86.tar.gz -C /usr/local/nodejs/
-	rm -rf node-v6.9.1-linux-x86.tar.gz
-	ln -s /usr/local/nodejs/node-v6.9.1-linux-x86/bin/node /usr/local/bin/node
-	ln -s /usr/local/nodejs/node-v6.9.1-linux-x86/bin/npm /usr/local/bin/npm
+	wget -N --no-check-certificate https://nodejs.org/dist/v6.9.1/node-v6.9.1-linux-x64.tar.gz
+ 	tar -xf node-v6.9.1-linux-x64.tar.gz -C /usr/local/nodejs/
+ 	rm -rf node-v6.9.1-linux-x64.tar.gz
+ 	ln -s /usr/local/nodejs/node-v6.9.1-linux-x64/bin/node /usr/local/bin/node
+ 	ln -s /usr/local/nodejs/node-v6.9.1-linux-x64/bin/npm /usr/local/bin/npm
 }
 install_libsodium(){
 	cd /root
