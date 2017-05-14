5c9528e01738716bd55bfc073a88fa326facb81f
Wing lee <mmmwhy@hust.edu.cn>
2017-05-14 13:52:34 +0800

    no message

---
+++ sm_node.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+#Check Root
+[ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
+#check OS version
+check_sys(){
+	if [[ -f /etc/redhat-release ]]; then
+		release="centos"
+	elif cat /etc/issue | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+	elif cat /proc/version | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /proc/version | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+    fi
+   # get_your_ip
+	IPAddress=`wget http://members.3322.org/dyndns/getip -O - -q ; echo`;
+}
+install_soft_for_each(){
+	check_sys
+	if [[ ${release} = "centos" ]]; then
+		yum groupinstall "Development Tools" -y
+		yum install -y wget curl tar unzip -y
+		yum install -y gcc gettext-devel unzip autoconf automake make zlib-devel libtool xmlto asciidoc udns-devel libev-devel vim
+		yum install -y pcre pcre-devel perl perl-devel cpio expat-devel openssl-devel mbedtls-devel screen nano
+	else
+		apt-get update
+		apt-get remove -y apache*
+		apt-get install -y build-essential npm wget curl tar git unzip gettext build-essential screen autoconf automake libtool openssl libssl-dev zlib1g-dev xmlto asciidoc libpcre3-dev libudns-dev libev-dev vim
+	fi
+}
+install_nodejs(){
+	mkdir /usr/local/nodejs
+ 	wget http://home.ustc.edu.cn/~mmmwhy/node-v6.9.1-linux-x64.tar.gz
+ 	tar -xf node-v6.9.1-linux-x64.tar.gz -C /usr/local/nodejs/
+ 	rm -rf node-v6.9.1-linux-x64.tar.gz
+ 	ln -s /usr/local/nodejs/node-v6.9.1-linux-x64/bin/node /usr/local/bin/node
+	ln -s /usr/local/nodejs/node-v6.9.1-linux-x64/bin/npm /usr/local/bin/npm
+	npm install -g cnpm --registry=https://registry.npm.taobao.org
+	ln -s /usr/local/nodejs/node-v6.9.1-linux-x64/bin/cnpm /usr/local/bin/cnpm
+}
+install_libsodium(){
+	wget -N -P  /root https://raw.githubusercontent.com/mmmwhy/ss-panel-and-ss-py-mu/master/libsodium-1.0.11.tar.gz
+	tar xvf libsodium-1.0.11.tar.gz && rm -rf libsodium-1.0.11.tar.gz
+	pushd libsodium-1.0.11
+	./configure --prefix=/usr && make
+	make install
+	popd
+	wget http://home.ustc.edu.cn/~mmmwhy/mbedtls-2.4.0-gpl.tgz
+	tar xvf mbedtls-2.4.0-gpl.tgz && rm -rf mbedtls-2.4.0-gpl.tgz
+	pushd mbedtls-2.4.0
+	make SHARED=1 CFLAGS=-fPIC
+	make DESTDIR=/usr install
+	popd
+	ldconfig
+}
+install_ss_libev(){
+	cd /root 
+	wget -N -P  /root https://raw.githubusercontent.com/mmmwhy/ss-mgr/master/shadowsocks-libev-3.0.3.tar.gz
+	tar -xf shadowsocks-libev-3.0.3.tar.gz && rm -rf shadowsocks-libev-3.0.3.tar.gz && cd shadowsocks-libev-3.0.3
+	./configure
+	make && make install
+}
+install_ss_mgr(){
+	install_soft_for_each
+	install_nodejs
+	install_libsodium
+	install_ss_libev
+	git clone https://github.com/mmmwhy/shadowsocks-manager.git "/root/shadowsocks-manager"
+	cd /root/shadowsocks-manager
+	cnpm i
+	ln -s /usr/local/nodejs/node-v6.9.1-linux-x64/bin/ssmgr /usr/local/bin/ssmgr
+	screen -dmS ss-manager ss-manager -m aes-256-cfb -u --manager-address 127.0.0.1:4000
+}
+ss_mgr_s(){
+	install_ss_mgr
+	mkdir /root/.ssmgr
+	sed -i "s#123456#${Usermukey}#g" /root/.ssmgr/webgui.yml
+	wget -N -P  /root/.ssmgr/ https://raw.githubusercontent.com/mmmwhy/ss-mgr/master/ss.yml
+	cd /root/shadowsocks-manager/
+	screen -dmS ss node server.js -c /root/.ssmgr/ss.yml
+}
+read -p "Please input your password(like:123456): " Usermukey
+Usermukey=${Usermukey:-"mupass"}
+ss_mgr_s
+iptables -I INPUT -p tcp -m tcp --dport 104 -j ACCEPT
+iptables -I INPUT -p tcp -m tcp --dport 1024: -j ACCEPT
+iptables-save
