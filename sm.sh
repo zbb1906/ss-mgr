d6da23a99b1208b391b3f6e444b56c6dc335f4c6
Wing lee <mmmwhy@hust.edu.cn>
2017-05-14 12:44:05 +0800

    no message

--- sm.sh
+++ sm.sh
@@ -18,8 +18,23 @@ check_sys(){
 	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
 		release="centos"
     fi
+   # get_your_ip
+	IPAddress=`wget http://members.3322.org/dyndns/getip -O - -q ; echo`;
 }
-install_node(){
+install_soft_for_each(){
+	check_sys
+	if [[ ${release} = "centos" ]]; then
+		yum groupinstall "Development Tools" -y
+		yum install -y wget curl tar unzip -y
+		yum install -y gcc gettext-devel unzip autoconf automake make zlib-devel libtool xmlto asciidoc udns-devel libev-devel vim
+		yum install -y pcre pcre-devel perl perl-devel cpio expat-devel openssl-devel mbedtls-devel screen nano
+	else
+		apt-get update
+		apt-get remove -y apache*
+		apt-get install -y build-essential npm wget curl tar git unzip gettext build-essential screen autoconf automake libtool openssl libssl-dev zlib1g-dev xmlto asciidoc libpcre3-dev libudns-dev libev-dev vim
+	fi
+}
+install_nodejs(){
 	mkdir /usr/local/nodejs
  	wget http://home.ustc.edu.cn/~mmmwhy/node-v6.9.1-linux-x64.tar.gz
  	tar -xf node-v6.9.1-linux-x64.tar.gz -C /usr/local/nodejs/
@@ -29,23 +44,7 @@ install_node(){
 	npm install -g cnpm --registry=https://registry.npm.taobao.org
 	ln -s /usr/local/nodejs/node-v6.9.1-linux-x64/bin/cnpm /usr/local/bin/cnpm
 }
-install_soft_for_each(){
-	check_sys
-	if [[ ${release} = "centos" ]]; then
-		yum groupinstall "Development Tools" -y
-		yum install -y wget curl tar unzip -y
-		yum install -y gcc gettext-devel unzip autoconf automake make zlib-devel libtool xmlto asciidoc udns-devel libev-devel vim
-		yum install -y pcre pcre-devel perl perl-devel cpio expat-devel openssl-devel mbedtls-devel screen nano
-		install_node
-	else
-		apt-get update
-		apt-get remove -y apache*
-		apt-get install -y build-essential npm wget curl tar git unzip gettext build-essential screen autoconf automake libtool openssl libssl-dev zlib1g-dev xmlto asciidoc libpcre3-dev libudns-dev libev-dev vim
-		install_node
-	fi
-}
 install_libsodium(){
-	install_soft_for_each
 	wget -N -P  /root https://raw.githubusercontent.com/mmmwhy/ss-panel-and-ss-py-mu/master/libsodium-1.0.11.tar.gz
 	tar xvf libsodium-1.0.11.tar.gz && rm -rf libsodium-1.0.11.tar.gz
 	pushd libsodium-1.0.11
@@ -61,7 +60,6 @@ install_libsodium(){
 	ldconfig
 }
 install_ss_libev(){
-	install_libsodium
 	cd /root 
 	wget -N -P  /root https://raw.githubusercontent.com/mmmwhy/ss-mgr/master/shadowsocks-libev-3.0.3.tar.gz
 	tar -xf shadowsocks-libev-3.0.3.tar.gz && rm -rf shadowsocks-libev-3.0.3.tar.gz && cd shadowsocks-libev-3.0.3
@@ -69,23 +67,31 @@ install_ss_libev(){
 	make && make install
 }
 install_ss_mgr(){
+	install_soft_for_each
+	install_nodejs
+	install_libsodium
 	install_ss_libev
 	git clone https://github.com/mmmwhy/shadowsocks-manager.git "/root/shadowsocks-manager"
 	cd /root/shadowsocks-manager
-	cnpm i
+	cnpm i -g shadowsocks-manager
+	ln -s /usr/local/nodejs/node-v6.9.1-linux-x64/bin/ssmgr /usr/local/bin/ssmgr
+	screen -dmS ss-manager ss-manager -m aes-256-cfb -u --manager-address 127.0.0.1:4000
 }
-install_ss_mgr
-# get_your_ip
-IPAddress=`wget http://members.3322.org/dyndns/getip -O - -q ; echo`;
-# node server.js
-mkdir /root/.ssmgr
-wget -N -P  /root/.ssmgr/ https://raw.githubusercontent.com/mmmwhy/ss-mgr/master/ss.yml
-sed -i "s#127.0.0.1#${IPAddress}#g" /root/.ssmgr/ss.yml
-screen -dmS ss-manager ss-manager -m aes-256-cfb -u --manager-address 127.0.0.1:4000
-cd /root/shadowsocks-manager/
-screen -dmS ss node server.js -c /root/.ssmgr/ss.yml
-wget -N -P  /root/.ssmgr/ https://raw.githubusercontent.com/mmmwhy/ss-mgr/master/webgui.yml
-sed -i "s#127.0.0.1#${IPAddress}#g" /root/.ssmgr/webgui.yml
-screen -dmS webgui node server.js -c /root/.ssmgr/webgui.yml
+ss_mgr_s(){
+	install_ss_mgr
+	mkdir /root/.ssmgr
+	wget -N -P  /root/.ssmgr/ https://raw.githubusercontent.com/mmmwhy/ss-mgr/master/ss.yml
+	#sed -i "s#127.0.0.1#${IPAddress}#g" /root/.ssmgr/ss.yml
+}
+ss_mgr_m(){
+	ss_mgr_s
+	cd /root/shadowsocks-manager/
+	screen -dmS ss node server.js -c /root/.ssmgr/ss.yml
+	wget -N -P  /root/.ssmgr/ https://raw.githubusercontent.com/mmmwhy/ss-mgr/master/webgui.yml
+	#sed -i "s#127.0.0.1#${IPAddress}#g" /root/.ssmgr/webgui.yml
+	screen -dmS webgui node server.js -c /root/.ssmgr/webgui.yml
+}
+ss_mgr_m
+iptables -I INPUT -p tcp -m tcp --dport 104 -j ACCEPT
 iptables -I INPUT -p tcp -m tcp --dport 1024: -j ACCEPT
 iptables-save
